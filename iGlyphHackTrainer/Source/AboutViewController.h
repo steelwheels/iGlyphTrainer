daddbb3c1dd7c71fe633fc274b96767c9b46af03
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-03-19 18:04:42 +0900

    iGlyphHackTrainer: Add "AboutViewController"

---
+++ iGlyphHackTrainer/Source/AboutViewController.h
@@ -0,0 +1,12 @@
+/**
+ * @file	AboutViewController.h
+ * @brief	Define AboutViewController class
+ * @par Copyright
+ *   Copyright (C) 2015 Steel Wheels Project
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ iGlyphHackTrainer/Source/AboutViewController.m
@@ -0,0 +1,12 @@
+/**
+ * @file	AboutViewController.m
+ * @brief	Define AboutViewController class
+ * @par Copyright
+ *   Copyright (C) 2015 Steel Wheels Project
+ */
+
+#import "AboutViewController.h"
+
+@implementation AboutViewController
+
+@end
