e8b5cdd4a5c0ff9d06d229894bbf5554a755f345
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-03-19 09:16:15 +0900

    iGlyphHackTrainer: Initial commit

---
+++ iGlyphHackTrainer/Source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iGlyphHackTrainer
+//
+//  Created by Tomoo Hamada on 2015/03/19.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iGlyphHackTrainer/Source/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iGlyphHackTrainer
+//
+//  Created by Tomoo Hamada on 2015/03/19.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
