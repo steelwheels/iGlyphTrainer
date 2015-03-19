2ae0e43f87810898b1110ea5e09158d477e46fee
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-03-19 21:19:31 +0900

    KGGameData: Initial commit

---
+++ KGGameData/Source/KGGameData.h
@@ -0,0 +1,19 @@
+//
+//  KGGameData.h
+//  KGGameData
+//
+//  Created by Tomoo Hamada on 2015/03/19.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KGGameData.
+FOUNDATION_EXPORT double KGGameDataVersionNumber;
+
+//! Project version string for KGGameData.
+FOUNDATION_EXPORT const unsigned char KGGameDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KGGameData/PublicHeader.h>
+
+
