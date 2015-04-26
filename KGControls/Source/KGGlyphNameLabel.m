daa15177899254d00fc6ed1f3ea3272f53a57097
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-04-26 19:24:32 +0900

    KGControls: Update to support new preference information

--- KGControls/Source/KGGlyphNameLabel.m
+++ KGControls/Source/KGGlyphNameLabel.m
@@ -8,6 +8,10 @@
 #import "KGGlyphNameLabel.h"
 #import <KGGameData/KGGameData.h>
 
+@interface KGGlyphNameLabel ()
+- (NSString *) glyphName: (KGGameStatus *) status ;
+@end
+
 @implementation KGGlyphNameLabel
 
 - (void) observeValueForKeyPath:(NSString *) keyPath ofObject:(id) object change:(NSDictionary *) change context:(void *) context
@@ -15,19 +19,7 @@
 	(void) change ; (void) context ;
 	if([object isKindOfClass: [KGGameStatus class]]){
 		if([keyPath isEqualToString: [KGGameStatus stateKeyPath]]){
-			
-			NSString * glyphname = @"" ;
-			KGGameStatus * status = object ;
-			switch(status.state){
-				case KGIdleState:
-				case KGStrokeState: {
-					
-				} break ;
-				case KGDisplayQuestionState:
-				case KGEvaluateState: {
-					glyphname = KGNameOfGlyph(status.currentGlyphKind) ;
-				} break ;
-			}
+			NSString * glyphname = [self glyphName: (KGGameStatus *) object] ;
 			dispatch_queue_t mainqueue = dispatch_get_main_queue();
 			dispatch_async(mainqueue, ^{
 #				if TARGET_OS_IPHONE
@@ -38,6 +30,32 @@
 			});
 		}
 	}
+}
+
+- (NSString *) glyphName: (KGGameStatus *) status
+{
+	KGPreference * preference = [KGPreference sharedPreference] ;
+	
+	NSString * glyphname = @"" ;
+	switch(status.state){
+		case KGIdleState: {
+			/* Display no glyph */
+		} break ;
+		case KGDisplayQuestionState: {
+			if(preference.doDisplayGlyphNameAtAnswerState){
+				glyphname = KGNameOfGlyph(status.currentGlyphKind) ;
+			}
+		} break ;
+		case KGInputAnswerState: {
+			if(preference.doDisplayGlyphNameAtAnswerState){
+				glyphname = KGNameOfGlyph(status.currentGlyphKind) ;
+			}
+		} break ;
+		case KGEvaluateState: {
+			glyphname = KGNameOfGlyph(status.currentGlyphKind) ;
+		} break ;
+	}
+	return glyphname ;
 }
 
 @end
