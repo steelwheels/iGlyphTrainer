56d5613146cb80f261f8cc0b7ac491af476efd89
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-05-02 07:46:35 +0900

    KGGlyphView: Add unit test for OSX

---
+++ KGGlyphView/UnitTest/Evaluate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UTEvaluate
+//
+//  Created by Tomoo Hamada on 2015/05/02.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ KGGlyphView/UnitTest/Evaluate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UTEvaluate
+//
+//  Created by Tomoo Hamada on 2015/05/02.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
