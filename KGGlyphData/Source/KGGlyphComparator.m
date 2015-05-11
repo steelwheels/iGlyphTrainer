759433e16b055869cae3a6a361ed4565e7e80acb
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-05-12 00:27:04 +0900

    KGGlyphData: Fix the glyph comparison function

--- KGGlyphData/Source/KGGlyphComparator.m
+++ KGGlyphData/Source/KGGlyphComparator.m
@@ -8,15 +8,15 @@
 #import "KGGlyphComparator.h"
 
 static void
-stroke2vertexes(int vertexes[], const struct KGGlyphStroke * src) ;
+stroke2vertexes(int vertexes[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM], const struct KGGlyphStroke * src) ;
 static int
-compareVertexes(int v0[], int v1[]) ;
+compareVertexes(int v0[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM], int v1[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM]) ;
 
 int
 KGCompareGlyphStrokes(const struct KGGlyphStroke * s0, const struct KGGlyphStroke * s1)
 {
-	int	vertexes0[KGGLYPH_VERTEX_NUM] ;
-	int	vertexes1[KGGLYPH_VERTEX_NUM] ;
+	int	vertexes0[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM] ;
+	int	vertexes1[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM] ;
 	
 	stroke2vertexes(vertexes0, s0) ;
 	stroke2vertexes(vertexes1, s1) ;
@@ -24,39 +24,38 @@ KGCompareGlyphStrokes(const struct KGGlyphStroke * s0, const struct KGGlyphStrok
 }
 
 static void
-stroke2vertexes(int vertexes[], const struct KGGlyphStroke * src)
+stroke2vertexes(int vertexes[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM], const struct KGGlyphStroke * src)
 {
 	/* Initialize the array */
-	unsigned int i ;
+	unsigned int i, j ;
 	for(i=0 ; i<KGGLYPH_VERTEX_NUM ; i++){
-		vertexes[i] = 0 ;
+		for(j=0 ; j<KGGLYPH_VERTEX_NUM ; j++){
+			vertexes[i][j] = 0 ;
+		}
 	}
 	
 	unsigned int count = src->edgeCount ;
 	for(i=0 ; i<count ; i++){
 		uint8_t fv = (src->edgeArray[i]).fromVertex ;
-		if(fv < KGGLYPH_VERTEX_NUM){
-			vertexes[fv] = 1 ;
-		} else {
-			fprintf(stderr, "%s: Invalid vertex id (%u)\n", __func__, fv) ;
-		}
 		uint8_t tv = (src->edgeArray[i]).toVertex ;
-		if(tv < KGGLYPH_VERTEX_NUM){
-			vertexes[tv] = 1 ;
+		if(fv < KGGLYPH_VERTEX_NUM && tv < KGGLYPH_VERTEX_NUM){
+			vertexes[fv][tv] = 1 ;
 		} else {
-			fprintf(stderr, "%s: Invalid vertex id (%u)\n", __func__, tv) ;
+			fprintf(stderr, "%s: Invalid vertex id (%u, %u)\n", __func__, fv, tv) ;
 		}
 	}
 }
 
 static int
-compareVertexes(int v0[], int v1[])
+compareVertexes(int v0[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM], int v1[KGGLYPH_VERTEX_NUM][KGGLYPH_VERTEX_NUM])
 {
-	unsigned int i ;
+	unsigned int i, j ;
 	for(i=0 ; i<KGGLYPH_VERTEX_NUM ; i++){
-		int diff = v0[i] - v1[i] ;
-		if(diff != 0){
-			return diff ;
+		for(j=0 ; j<KGGLYPH_VERTEX_NUM ; j++){
+			int diff = v0[i][j] - v1[i][j] ;
+			if(diff != 0){
+				return diff ;
+			}
 		}
 	}
 	return 0 ;
