8f03ec577243cf88558869e0126f7534d8d7ea07
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-10-08 01:13:48 +0900

    all: Remove all file to implement by Swift

--- KGGlyphData/Source/KGGlyphModel.m
+++
@@ -1,8 +0,0 @@
-/**
- * @file	KGGlyphModel.m
- * @brief	Model of glyph
- * @par Copyright
- *   Copyright (C) 2015 Steel Wheels Project
- */
-
-#import "KGGlyphModel.h"
--- KGGlyphView/Source/KGGlyphView.m
+++
@@ -1,8 +0,0 @@
-/**
- * @file	KGGlyphView.m
- * @brief	Define KGGlyphView data structure
- * @par Copyright
- *   Copyright (C) 2015 Steel Wheels Project
- */
-
-#import "KGGlyphView.h"
