3ef11a70f1dad3705970878dea8c5b4062c141bf
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-12-22 01:10:28 +0900

    iGlyphTrainer: Initial commit

--- GTGameData/Source/Preference/GTColorPreference.swift
+++ GTGameData/Source/Preference/GTColorPreference.swift
@@ -10,15 +10,12 @@ import KiwiGraphics
 
 public class GTColorPreference
 {
-	static let sharedTable = GTColorPreference()
-
-	public init(){
-	}
-
 	public static let questionProgressColor: CGColor	= KGColorTable.gold.cgColor
 	public static let answerProgressColor: CGColor		= KGColorTable.cyan.cgColor
 	public static let progressStrokeWidth: CGFloat		= 4.0
 
+	public static let timerFontColor:CGColor		= KGColorTable.gold.cgColor
+	
 	public static let glyphVertexColor: CGColor		= KGColorTable.white.cgColor
 	public static let glyphStrokeWidth: CGFloat		= 10.0
 	public static let glyphStrokeColor: CGColor		= KGColorTable.gold.cgColor
---
+++ iGlyphTrainer/Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iGlyphTrainer
+//
+//  Created by Tomoo Hamada on 2016/12/22.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
